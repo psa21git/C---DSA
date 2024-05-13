aca80ffb65d57fd612371dbce4c8439a659f2cb7
psa_code <p.sreyanshuspazz@gmail.com>
2024-05-13 21:06:04 +0530

    Day 0

---
+++ Supreme 3.0/CPPCRASH/User Input/usr.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Entered age is : " << age << endl;
+
+    // boolean has to be entered as 1 and 0 and not as true or false
+
+
+    // Multiple Inputs
+    int m1,m2,m3;
+    cout << "Enter marks of 3 subjects: ";
+    cin >> m1 >> m2 >> m3;
+    cout << "Marks of 3 subjects are: " << m1 << ", " << m2 << ", " << m3;
+
+    return 0;
+}
